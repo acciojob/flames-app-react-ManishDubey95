7fb5cf8bca3c2aac3305999dd41651c8869ace5e
ManishDubey95 <136079775+ManishDubey95@users.noreply.github.com>
2023-12-19 14:48:49 +0000

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,73 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, {useState} from "react";
 
-class App extends Component {
-    render() {
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
+let arr = ["Siblings", "Friends", "Love", "Affection", "Marriage", "Enemy"]
+const App = () => {
+
+    const [name1, setName1] = useState("");
+    const [name2, setName2] = useState("");
+    const [relation, setRelation] = useState("");
+    // const [btnClicked, setBtnClicked] = useState(false);
+
+    function findRelation(event) {
+        event.preventDefault();
+
+        if(name1.trim() === "" || name2.trim() === ""){
+            // setBtnClicked(false);
+            setRelation("Please Enter Valid Names");
+            return;
+        }
+
+        let str1 = name1
+        let str2 = name2
+        for(let ch of str1){
+            if(str2.includes(ch)){
+                str1 = str1.replace(ch, "");
+                str2 = str2.replace(ch, "");
+            }
+        }
+        setName1(str1);
+        setName2(str2);
+        // setBtnClicked(true);
+        setRelation(arr[(str1.length + str2.length)%6]);
     }
+    
+
+
+    return(
+        <div id="main">
+            <form>
+               <input data-testid="input1" type="text" placeholder="Enter First Name" name="name1"
+               onChange={(event) => setName1(event.target.value)}
+               value={name1} 
+               />
+
+               <input data-testid="input2" type="text" placeholder="Enter Second Name" name="name2"
+               onChange={(event) => setName2(event.target.value)}
+               value={name2} 
+               />
+               
+               <button data-testid="calculate_relationship" type="submit"
+               onClick={findRelation}>Calculate Relationship Future</button>
+               
+               <button data-testid="clear" type="reset" 
+               onClick={() =>{
+                setName1("")
+                setName2("")
+                setRelation("")
+                // setBtnClicked(false)
+               }}
+               >Clear</button>
+            </form>
+
+            <h3 data-testid="answer">
+                {
+                    // btnClicked && (arr[(str1.length + str2.length)%6])
+                    relation
+                }
+            </h3>
+        </div>
+    )
 }
 
 
